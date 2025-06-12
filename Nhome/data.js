618f0b46d4c1a65dde9dedddb3c91b27f68e73dd
Rex-10735 <classforlph@gmail.com>
2025-06-12 20:57:38 +0800

    0

---
+++ Nhome/data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "海科館首頁",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.6804653283595785,
+        "pitch": -0.07051461067429798,
+        "fov": 1.1484357166903083
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 1.3844857174208567,
+          "pitch": -0.17584417076914782,
+          "title": "<span style=\"color: rgb(51, 51, 51); font-family: Arial, Helvetica, 微軟正黑體, sans-serif; font-size: 15.84px; text-align: justify; background-color: rgb(255, 255, 255);\">國立海洋科技博物館</span>",
+          "text": "<span style=\"color: rgb(51, 51, 51); font-family: Arial, Helvetica, 微軟正黑體, sans-serif; font-size: 15.84px; text-align: justify; background-color: rgb(255, 255, 255);\">以學校團體和家庭團體為目標觀眾，是一座兼具展示、教育、研究、蒐藏、休閒娛樂功能，以激發觀眾「親近海洋、認識海洋、善待海洋」，並使「海洋」得以永續發展為使命的博物館。</span>"
+        }
+      ]
+    }
+  ],
+  "name": "海科館首頁",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
---
+++ Nup/data.js
@@ -0,0 +1,240 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "海科館正門",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 0.35259763586115866,
+        "pitch": -0.01834200243415296,
+        "fov": 1.1484357166903083
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.10041766166969701,
+          "pitch": 0.25481562492873167,
+          "rotation": 0,
+          "target": "1--1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5989881898695,
+          "pitch": -0.1947126673406494,
+          "title": "海科館-主題館",
+          "text": "<br><div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "1--1",
+      "name": "海科館走道-1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 2.8251940820249235,
+        "pitch": 0.17530306484430902,
+        "fov": 1.1484357166903083
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.32439527649354005,
+          "pitch": 0.19361620734601104,
+          "rotation": 0,
+          "target": "2--"
+        },
+        {
+          "yaw": 2.8398456035368387,
+          "pitch": 0.17120144413480531,
+          "rotation": 0,
+          "target": "3--2"
+        },
+        {
+          "yaw": -2.0499796227367124,
+          "pitch": 0.23306505239484565,
+          "rotation": 6.283185307179586,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2--",
+      "name": "海科館廣場-上",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -2.952770503718355,
+        "pitch": 0.11862945560491056,
+        "fov": 1.1484357166903083
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.998600984404675,
+          "pitch": 0.21333856884480262,
+          "rotation": 0,
+          "target": "1--1"
+        },
+        {
+          "yaw": -0.38300083226579495,
+          "pitch": 0.1927690200784209,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.1657762205859843,
+          "pitch": -0.06031470314814236,
+          "title": "海洋劇場",
+          "text": "<span style=\"color: rgb(51, 51, 51); font-family: Arial, Helvetica, 微軟正黑體, sans-serif; font-size: 15.84px; text-align: justify; background-color: rgb(255, 255, 255);\">海洋劇場建築外型設計構想來自「海洋」與「洋流」，屋頂是特殊的波浪造型，外牆則是利用抿石子施工手法，營造出一片片魚鱗的感覺</span>"
+        }
+      ]
+    },
+    {
+      "id": "3--2",
+      "name": "海科館走道-2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -0.13638229964117166,
+        "pitch": 0.2400666413991548,
+        "fov": 1.1484357166903083
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3553397221845209,
+          "pitch": 0.28077872408351645,
+          "rotation": 0,
+          "target": "1--1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-",
+      "name": "海科館八斗街側門",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -2.2694868797908523,
+        "pitch": 0.1092168426223239,
+        "fov": 1.1484357166903083
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1249644551830134,
+          "pitch": 0.14326427006352738,
+          "rotation": 0,
+          "target": "2--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.156447660216946,
+          "pitch": -0.09914518853393517,
+          "title": "海科館-探索館",
+          "text": "<br><div><br></div>"
+        }
+      ]
+    }
+  ],
+  "name": "海科館上層",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};

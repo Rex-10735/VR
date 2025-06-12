cdd58eed51184645901898bb9bfde91052354ae1
Rex-10735 <classforlph@gmail.com>
2025-06-12 21:43:48 +0800

    0

---
+++ Ndown/data.js
@@ -0,0 +1,400 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0--",
+      "name": "海科館廣場-下",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 3.1217474802213347,
+        "pitch": 0.03911546955740164,
+        "fov": 1.1484357166903083
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.855555216771144,
+          "pitch": 0.12518130021685536,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 0.15476283426876059,
+          "pitch": 0.15286536503583825,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 2.474296582817858,
+          "pitch": 0.023328345528849326,
+          "rotation": 0,
+          "target": "5--1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.0469237127368025,
+          "pitch": 0.02570228272018582,
+          "title": "禮品店",
+          "text": "<br><div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "海科館碧水巷廣場",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -2.4361677527898884,
+        "pitch": 0.1879087214455808,
+        "fov": 1.1484357166903083
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9795093508051789,
+          "pitch": 0.1891308409463459,
+          "rotation": 0,
+          "target": "0--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.140508785601561,
+          "pitch": 0.2964947228233452,
+          "title": "海科館碧水巷廣場",
+          "text": "<span style=\"color: rgb(51, 51, 51); font-family: Arial, Helvetica, 微軟正黑體, sans-serif; font-size: 15.84px; text-align: justify; background-color: rgb(255, 255, 255);\">海科館構築在北部火力發電廠原址，該址原為一狹長海溝，當地人或稱為「碧水巷」，分隔「八斗子島」與「臺灣島」。為保留歷史記憶，現今主題館建築物西南側中央的活動廣場，即「碧水巷」原址，命名為「碧水巷廣場」，以保留對地理歷史文化的足跡。</span>"
+        }
+      ]
+    },
+    {
+      "id": "2-2",
+      "name": "海科館2樓售票處",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -2.7224017368883864,
+        "pitch": 0.18284764898695371,
+        "fov": 1.1484357166903083
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.761716506266371,
+          "pitch": 0.1645797326192664,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.754124597170417,
+          "pitch": 0.03234230506753377,
+          "title": "售票處",
+          "text": "販售主題館各展廳門票"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "海科館一樓大廳",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": 0.02500692452915132,
+        "pitch": 0.2567160869112364,
+        "fov": 1.1484357166903083
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.23548167543389198,
+          "pitch": 0.2479681248406962,
+          "rotation": 0,
+          "target": "2-2"
+        },
+        {
+          "yaw": -2.88688546380725,
+          "pitch": 0.32389793624337315,
+          "rotation": 0,
+          "target": "0--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.012256874917266458,
+          "pitch": 0.5819318002859521,
+          "title": "指示牌",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "海科館長潭里漁港側門",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -2.8558452111082673,
+        "pitch": 0.1581069060864131,
+        "fov": 1.1484357166903083
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.11374793663692451,
+          "pitch": 0.23748645135882462,
+          "rotation": 0,
+          "target": "6--2"
+        },
+        {
+          "yaw": -2.951732641216841,
+          "pitch": 0.14243522681169374,
+          "rotation": 1.5707963267948966,
+          "target": "7-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.6206935568747287,
+          "pitch": -0.017557340344566796,
+          "title": "長潭里漁港",
+          "text": "<br>"
+        }
+      ]
+    },
+    {
+      "id": "5--1",
+      "name": "海科館廊道-1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -3.0824060014660724,
+        "pitch": 0.1410706004469553,
+        "fov": 1.1484357166903083
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.21167737866881708,
+          "pitch": 0.1633532890960332,
+          "rotation": 0,
+          "target": "0--"
+        },
+        {
+          "yaw": -3.134276572021129,
+          "pitch": 0.2324283463961141,
+          "rotation": 0,
+          "target": "6--2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6--2",
+      "name": "海科館廊道-2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1664,
+      "initialViewParameters": {
+        "yaw": -2.931627696091155,
+        "pitch": 0.156368716208501,
+        "fov": 1.1484357166903083
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1147116635864087,
+          "pitch": 0.15004733569123196,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": -0.007467645707095016,
+          "pitch": 0.23022161862292556,
+          "rotation": 0,
+          "target": "5--1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.124147735873855,
+          "pitch": 0.010967941339625753,
+          "title": "遺址廣場",
+          "text": "<span style=\"color: rgb(51, 51, 51); font-family: Arial, Helvetica, 微軟正黑體, sans-serif; font-size: 15.84px; text-align: justify; background-color: rgb(255, 255, 255);\">在廣場內的生態水池旁有兩座混凝土構造物物矗立在旁，該構造物原為1955年完成之北部火力發電廠擴建工程之兩座發電機基座。為保留歷史記憶，將此小廣場命名為「遺址廣場」。</span>"
+        }
+      ]
+    },
+    {
+      "id": "7-",
+      "name": "海科館北火熊",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.5904404000545993,
+        "pitch": 0.19075682531520322,
+        "fov": 1.1484357166903083
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.359483309094049,
+          "pitch": 0.19886991152820244,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.545078623389351,
+          "pitch": -0.050996824503448224,
+          "title": "北火熊",
+          "text": "<span data-sheets-root=\"1\" style=\"color: rgb(0, 0, 0); font-size: 12pt; font-family: Arial; font-weight: bold; text-align: center;\">「北火熊」是海科館的吉祥物，肚上的火焰是他的識別標誌</span>"
+        }
+      ]
+    }
+  ],
+  "name": "海科館下層",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
